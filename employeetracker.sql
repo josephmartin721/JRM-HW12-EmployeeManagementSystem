ceafdc8a4080a14094b6a7bc17e2813953f34cd6
josephmartin721 <josephmartin721@yahoo.com>
2021-04-15 14:54:00 -0500

    pseudo-code for databse and table creation

---
+++ employeetracker.sql
@@ -0,0 +1,11 @@
+-- Create and set default database --
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- Department table --
+
+-- Role table --
+
+-- Employee table --
